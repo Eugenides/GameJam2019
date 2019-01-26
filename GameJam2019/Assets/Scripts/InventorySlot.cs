8310c0df436d973147571157a8478594a7c8b32d
NicolasAmaya <32581751+NicolasAmaya@users.noreply.github.com>
2019-01-26 15:50:24 -0500

    UI changes

---
+++ GameJam2019/Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour {
+
+    //TODO: VAR ITEM OR ITEM TYPE
+    GameObject item;//this is temporal until we get the right var
+    public Image icon;
+
+    public Button removeButton;
+
+    public void AddItem(GameObject newItem/*Item newItem*/)
+    {
+
+        item = newItem;
+
+        //icon.sprite = item.icon;
+        icon.enabled = true;
+        removeButton.interactable = true;
+    }
+
+    public void ClearSlot()
+    {
+
+        item = null;
+
+        icon.sprite = null;
+        icon.enabled = false;
+        removeButton.interactable = false;
+    }
+
+    public void OnRemoveButton() {
+
+        //Inventory.Remove(item);
+        Debug.Log("removing item");
+    }
+
+    public void UseItem()
+    {
+        /* if(item != null)
+         {
+
+             item.Use();
+             //create a use function in item class 
+         }*/
+        Debug.Log("use item");
+
+    }
+}
---
+++ GameJam2019/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour {
+    //TO DO: GET INVENTORY 
+    //Inventory inventory;
+    // Use this for initialization
+    public Transform itemsParent;
+
+    InventorySlot[] slots;
+
+    public GameObject inventoryUI;
+    public GameObject inventoryUIClosed;
+
+
+	void Start () {
+
+        //TO DO: ASSIGN INVETORY  
+        //inventory = INventory.instance
+        //inventory.onItemchangeCallback += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //toggleUI
+        if (Input.GetButtonDown("Inventory"))
+        {
+
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+            //TODO toggle on the smalll version of inventory with weight 
+            inventoryUIClosed.SetActive(!inventoryUI.activeSelf);
+        }
+
+	}
+
+    void UpdateUI()
+    {
+        Debug.Log("Update UI");
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+
+            /*if(i < inventory.items.Count){
+
+                slots[i].ClearSlot();}*/
+        }
+    }
+}
