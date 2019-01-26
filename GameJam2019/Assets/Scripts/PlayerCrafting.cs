44ad31612b17ff814e5fb4d8a6f57b831dfcc932
DESKTOP-2TTVG4M\grump <grumpyonion82@hotmail.com>
2019-01-26 15:23:57 -0500

    Finished resources with images. Began creating simple crafting menu and craftable class

---
+++ GameJam2019/Assets/Scripts/PlayerCrafting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCrafting : MonoBehaviour {
+
+    //Reference to the image panel that holds the list of craftable items.
+    public Image craftingPanel;
+
+    //Reference to the player object's inventory.
+    private PlayerInventory pInventory;
+
+	// Use this for initialization
+	void Start () {
+        pInventory = GetComponent<PlayerInventory>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Enables the panel on screen to be seen or hidden.
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (craftingPanel.enabled == true)
+                craftingPanel.enabled = false;
+            else
+                craftingPanel.enabled = true;
+        }
+	}
+
+    public void CraftCraftingBench()
+    {
+
+    }
+}
